50470a92d8bb0af7e2ee372260277dde3f5d3e30
Javier Salazar <40800436+zombiewafle@users.noreply.github.com>
2018-08-29 17:51:08 -0600

    Add files via upload

--- ListasDeArticulos/src/sample/Main.java
+++ ListasDeArticulos/src/sample/Main.java
@@ -6,15 +6,13 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.util.ArrayList;
-
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Control de Listas de Compra");
-        primaryStage.setScene(new Scene(root, 400, 375));
+        primaryStage.setScene(new Scene(root, 450, 375));
         primaryStage.show();
     }
 
